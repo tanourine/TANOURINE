02b48436117dd7d690cf78e5ff8157afd441f296
Ali Hoseen <ali.hoseen@example.comtaltrmous@gmail.com>
2025-06-19 14:34:42 +0400

    أول نسخة من تطبيق تنورين

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text('تنورين للتكييف')),
+        body: Center(child: Text('تطبيق تنورين - جاري الإعداد')),
+      ),
+    );
+  }
+}
