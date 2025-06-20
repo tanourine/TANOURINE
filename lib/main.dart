cbec179a3b7a36ab41fd8eec7d7159d6653ff3db
tanourine <taltrmous@gmail.com>
2025-06-20 14:50:13 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,1 @@
-import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text('تنورين للتكييف')),
-        body: Center(child: Text('تطبيق تنورين - جاري الإعداد')),
-      ),
-    );
-  }
-}
